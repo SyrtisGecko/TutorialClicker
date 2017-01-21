3e8fdd846d13036da0054e99ca6ed628fe54d8ac
SyrtisGecko <syrtismajor@gmail.com>
2017-01-21 21:11:30 +0100

    Displaying Player Image

--- core/src/screens/GameplayScreen.java
+++ core/src/screens/GameplayScreen.java
@@ -1,8 +1,11 @@
 package screens;
 
 import com.mygdx.game.tut.TutorialClickerGame;
+import com.mygdx.game.tut.entities.Player;
 
 public class GameplayScreen extends AbstractScreen {
+
+    private Player player;
 
     public GameplayScreen(TutorialClickerGame game) {
         super(game);
@@ -10,7 +13,26 @@ public class GameplayScreen extends AbstractScreen {
     }
 
     private void init() {
+        initPlayer();
+    }
 
+    private void initPlayer() {
+        player = new Player();
+        stage.addActor(player);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update();
+
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+    }
+
+    private void update() {
+        stage.act();
     }
 
     @Override
