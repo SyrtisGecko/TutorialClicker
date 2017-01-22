5cf5e9401e71787a03ebd96cc068b1e4d3c9fb30
SyrtisGecko <syrtismajor@gmail.com>
2017-01-23 00:10:45 +0100

    Added ResetScoreButton asset with atlas

--- core/src/ui/ResetScoreButton.java
+++ core/src/ui/ResetScoreButton.java
@@ -1,7 +1,10 @@
 package ui;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 /**
@@ -10,7 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 public class ResetScoreButton extends Button {
 
     public ResetScoreButton(final IClickCallback callback) {
-        super(new ButtonStyle());
+        super(prepareResetButtonStyle());
 
         init(callback);
 
@@ -30,6 +33,16 @@ public class ResetScoreButton extends Button {
                 return super.touchDown(event, x, y, pointer, button);
             }
         });
+    }
+
+    private static ButtonStyle prepareResetButtonStyle() {
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("ui-red.atlas"));
+        Skin skin = new Skin(atlas);
+        ButtonStyle buttonStyle = new ButtonStyle();
+        buttonStyle.up = skin.getDrawable("button_02");
+        buttonStyle.down = skin.getDrawable("button_03");
+
+        return buttonStyle;
     }
 
 }
