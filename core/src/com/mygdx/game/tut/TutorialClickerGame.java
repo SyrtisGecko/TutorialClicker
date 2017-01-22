1488326427e1d3c06cbdc8442c58848300454d44
SyrtisGecko <syrtismajor@gmail.com>
2017-01-22 13:03:12 +0100

    Added saving/loading score in TutorialClickerGam

--- core/src/com/mygdx/game/tut/TutorialClickerGame.java
+++ core/src/com/mygdx/game/tut/TutorialClickerGame.java
@@ -3,12 +3,16 @@ package com.mygdx.game.tut;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import screens.SplashScreen;
 
 public class TutorialClickerGame extends Game {
+
+	public final static String GAME_PREFS = "com.mygdx.game.tut.prefs";
+	public final static String GAME_SCORE = "com.mygdx.game.tut.prefs.score";
 
 	public final static String GAME_NAME = "Tutorial Clicker";
 
@@ -17,16 +21,30 @@ public class TutorialClickerGame extends Game {
 
 	private boolean paused;
 
+	private Preferences prefs;
+
 	private int points;
 
 	
 	@Override
 	public void create () {
+		init();
 		this.setScreen(new SplashScreen(this));
+	}
+
+	private void init() {
+		prefs = Gdx.app.getPreferences(GAME_PREFS);
+		loadScore();
+	}
+
+	private void loadScore() {
+		points = prefs.getInteger(GAME_SCORE);
 	}
 
 	public void addPoint() {
 		points++;
+		prefs.putInteger(GAME_SCORE, points);
+		prefs.flush();
 	}
 
 	/**
