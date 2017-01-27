806f44827be7ce01e98506f26d0aab2e162aebec
SyrtisGecko <syrtismajor@gmail.com>
2017-01-27 07:59:30 +0100

    Added some random events

--- core/src/com/mygdx/game/tut/controllers/RandomEventController.java
+++ core/src/com/mygdx/game/tut/controllers/RandomEventController.java
@@ -2,12 +2,16 @@ package com.mygdx.game.tut.controllers;
 
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Timer;
+import com.mygdx.game.tut.TutorialClickerGame;
 
 public class RandomEventController {
 
     private final int RANDOM_TICK_INTERVAL = 5; // TODO change after implementation
 
-    public RandomEventController() {
+    private TutorialClickerGame game;
+
+    public RandomEventController(TutorialClickerGame game) {
+        this.game = game;
         init();
     }
 
@@ -24,5 +28,34 @@ public class RandomEventController {
 
     private void triggerRandomEvent() {
         System.out.println("Random event triggered.");
+        int randomNumber = MathUtils.random(1, 3);
+        switch (randomNumber) {
+            case 1:
+                gainMoneyEvent();
+                break;
+            case 2:
+                looseMoneyEvent();
+                break;
+            case 3:
+                gainPassiveIncome();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void gainPassiveIncome() {
+        game.getScoreService().addPassiveIncome();
+        System.out.println("Gained passive income.");
+    }
+
+    private void looseMoneyEvent() {
+        game.getScoreService().addPoints(-123);
+        System.out.println("Lost money.");
+    }
+
+    private void gainMoneyEvent() {
+        game.getScoreService().addPoints(123);
+        System.out.println("Gained money.");
     }
 }
